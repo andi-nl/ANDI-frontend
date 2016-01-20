8b1df945ad65138e470b0f7e5948f4cf17184f0a
Anand Gavai <anand.gavai@gmail.com>
2016-01-20 18:50:53 +0100

    specific selection of leaf nodes

--- app/scripts/controllers/diagnosis_v2.js
+++ app/scripts/controllers/diagnosis_v2.js
@@ -1127,6 +1127,9 @@ var tests =
 
 app.controller("PanelController",function(){
   this.tab=1;
+    
+   
+
   this.selectTab=function(setTab){
     this.tab=setTab;
   };
@@ -1153,7 +1156,6 @@ app.controller('TableController', function($scope) {
   var dum={};
   var tree;
   this.tree_data = tests;
-  this.submitted = false;
   this.my_tree = tree = {};
   this.expanding_property = {
       field: "label",
@@ -1172,7 +1174,7 @@ app.controller('TableController', function($scope) {
       console.log('you clicked on', branch)
   }
   this.addPatient=function(){
-      if(this.counter<6){
+      if(this.counter<4){
         this.counterlimit++;
         this.tabledata[this.counterlimit] = 'Table '+this.counterlimit;
         this.counter++;
@@ -1190,7 +1192,7 @@ app.controller('TableController', function($scope) {
   };
 
   this.calculateAge = function (birthday) { // birthday is a date
-    //var ageDifMs = Date.now() - birthday.getTime();
+    var ageDifMs = Date.now() - birthday.getTime();
     var ageDifMs = Date.now();
     var ageDate = new Date(ageDifMs); // miliseconds from epoch
     return Math.abs(ageDate.getUTCFullYear() - 1970);
@@ -1198,11 +1200,12 @@ app.controller('TableController', function($scope) {
 
 
   this.createPatientObject=function(){
-   var age= this.calculateAge();
-   dum= {"patient.id":$scope.patient.id,
+   var age= this.calculateAge($scope.patient[0].age);
+   dum= {"patient.id":$scope.patient[0].id,
          "patient.age":age,
-         "patient.sex":$scope.patient.sex,
-         "patient.education":$scope.patient.education
+         "patient.sex":$scope.patient[0].sex,
+         "patient.education":$scope.patient[0].education,
+         "patient.test":$scope.patient[0].test
         };
    return dum;
   };
@@ -1211,42 +1214,18 @@ app.controller('TableController', function($scope) {
    return $scope.patient.test.value;
   };
 
-  this.exportCSV=function(){};
-
-
   this.getPat=function(){
-    return ($scope.patient.id);
+    dum= {
+      "patient":$scope.patient
+    };
+    return(dum.patient[0].sex);
   };
 
-  this.submit=function(isValid){
-            
-  // check to make sure the form is completely valid
-    if ($scope.patient.form.$invalid) {
-
-    }
-    else{
-      var limit = 1;
-      //for(var i=0;i<(this.counter-1);i++){
-      for (var i in $scope.patient) {
-        if(limit<this.counter){
-          var patientTest = [];
-          for (var key in $scope.patient[i].test) {
-            if ($scope.patient[i].test.hasOwnProperty(key)) {
-              for (var key1 in $scope.patient[i].test[key]) {
-                if ($scope.patient[i].test[key].hasOwnProperty(key1)) {
-                  patientTest.push({id:key,label:key1,value:$scope.patient[i].test[key][key1]});
-                }
-              }
-            }
-          }
-          $scope.patient[i].test= patientTest;
-          limit++;
-        }
-      }
-      console.log($scope.patient);
-    } 
-
+  this.submit=function(){
+    console.log($scope.patient);
   };
+
+
 });
 
 app.controller('treeController', function() {
@@ -1259,13 +1238,10 @@ app.controller('treeController', function() {
     // Do soemthing with node or tree based on isSelected
   };
   this.getSelectedNodes=function(node){
-    //return node.label;
-    if (node.isSelected==true || node.children.length>0) {
+    if (node.isSelected==true) {
       return node.label;
     };
   };
- 
-
   this.getjson=function (tests) {
      var k =JSON.stringify(tests);
      return k;
@@ -1275,7 +1251,6 @@ app.controller('treeController', function() {
       return node.id;
     };
   };
-
   /// Important function for tree traversal 
   var arr=[];
   this.process = function(key,value) {
@@ -1338,7 +1313,6 @@ app.controller('treeController', function() {
   // ENDs here make sure you adapt it :-)
 });
 
-
 var pat = [{
   "patient.id":9999,
   "patient.age":"12/12/1975",
@@ -1352,253 +1326,6 @@ var pat = [{
 }
 ];
 
-// this controller is not in use
-/*app.controller('treeCtrl', function ($scope) {
-
-$scope.data =
-[
-    { "name" : "Intelligence tests / premorbid IQ tests", "children" : [
-          { "name" : "Dutch Adult Reading Test (DART/NART)", "children" : [] 
-          },
-          { "name" : "Wechsler Adult Intelligence Scale", "children" : [] 
-          },
-          { "name" : "WAIS-NL (1970)","children": [
-            {"name": "WAIS_NL_Digitspan","children":[]},
-            {"name": "WAIS_NL_Information","children":[]},
-            {"name": "WAIS_NL_arithmetic","children":[]},
-            {"name": "WAIS_NL_Similarities","children":[]},
-            {"name": "WAIS_NL_Coding","children":[]},
-            {"name": "WAIS_NL_PictureCompletion","children":[]},
-            {"name": "WAIS_NL_BlockDesign","children":[]}
-          ] 
-          },
-          { "name" : "WAIS-R","children" : 
-            [
-                  {"name" : "WAIS_R_Digitspan", "children" : []},
-                  {"name" : "WAIS_R_Coding", "children" : []}
-            ]
-        },
-        { "name" : "WAIS-III_NL","children": [
-            {"name": "WAIS_III_NL_VisualPuzzle","children":[]},
-            {"name": "WAIS_III_NL_Comprehension","children":[]},
-            {"name": "WAIS_III_NL_SymbolSearch","children":[]},
-            {"name": "WAIS_III_NL_Information","children":[]},
-            {"name": "WAIS_III_NL_Arithmetic","children":[]},
-            {"name": "WAIS_III_LeNoSeq","children":[]},
-            {"name": "WAIS_III_NL_PictureComplet","children":[]},
-            {"name": "WAIS_III_NL_Similarities","children":[]},
-            {"name": "WAIS_III_NL_Coding","children":[]},
-            {"name": "WAIS_III_NL_Digitspan","children":[]},
-            {"name": "WAIS_III_NL_LetterNumber_Sequencing","children":[]},
-            {"name": "WAIS_III_NL_MatrixReasoning","children":[]},
-            {"name": "WAIS_III_NL_BlockDesign","children":[]},
-            {"name": "WAIS_III_NL_Vocabulary","children":[]}            
-          ] 
-          },
-          { "name" : "WAIS-IV_NL", "children" : 
-              [
-                  { "name" : "WAIS-IV_Digitspan", "children" : [] },
-                { "name" : "WAIS-IV Picture Completion", "children" : [] }
-              ]
-            },   
-            {"name": "Wechsler Memory Scales","children":[]},
-            {"name": "WMS-R_NL","children":[
-                          {"name": "WMS_R_NL_WordPairs","children":[]},
-                          {"name": "WMS_R_NL_VisualRepro","children":[]},
-                          {"name": "WMS_R_NL_Digitspan","children":[]},
-                          {"name": "WMS_R_NL_PatternRecognition","children":[]},
-                          {"name": "WMS_R_NL_VisualPairedAssociation","children":[]}
-           ]},
-          { "name" : "WMS-III_NL", "children" : 
-              [
-                  { "name" : "WMS_III_NL_SpatialSpan", "children" : [] },
-                { "name" : "WMS_III_NL_FaceRecog", "children" : [] }
-              ]
-            },
-            {"name": "Coding Task","children":[]}, 
-            {"name": "Wechsler Intelligence Scale Children","children":[]}, 
-            {"name": "WISC_NL","children":[
-              {"name": "WISC_NL_Mazes","children":[]}, 
-            ]},
-            {"name": "WISC-R_NL","children":[
-              {"name": "WISC_R_NL_plaatjes","children":[]}, 
-              {"name": "WISC_R_NL_rekenen","children":[]}, 
-              {"name": "WISC_R_NL_blokpatronen","children":[]}, 
-              {"name": "WISC_R_NL_woordenschat","children":[]}, 
-              {"name": "WISC_R_NL_total","children":[]}, 
-              {"name": "WISC_R_NL_Mazes","children":[]}, 
-              ]
-          }, 
-            {"name": "WISC-III_NL","children":[
-              {"name": "WISC_III_NL_SymbolSubstitution","children":[]}, 
-              {"name": "WISC_III_NL_plaatjes","children":[]}, 
-              {"name": "WISC_III_NL_blokpatronen","children":[]}, 
-              {"name": "WISC_III_NL_rekenen","children":[]}, 
-              {"name": "WISC_III_NL_woordkennis","children":[]}, 
-              {"name": "WISC_III_NL_symbolcomparisson","children":[]},
-              {"name": "WISC_III_NL_digitspan","children":[]}, 
-              ]
-          }, 
-            {"name": "Groninger Intelligentie Test","children":[]}, 
-            {"name": "GIT1","children":[
-              {"name": "GIT1_Legkaarten","children":[]}, 
-              {"name": "GIT1_Matrijzen","children":[]}, 
-              {"name": "GIT1_Woordopnoemen","children":[]}, 
-              {"name": "GIT1_Cijferen","children":[]},
-              {"name": "GIT1_Lijntekeningen","children":[]}
-            ]}, 
-            {"name": "Raven Matrices","children":[]}, 
-            {"name": "Raven Coloured Progressive Matrices (RCPM)","children":[]}, 
-            {"name": "Raven Standard Progressive Matrices (RSPM)","children":[]}, 
-            {"name": "Differentiële Aanleg Testserie (DAT):","children":[
-              {"name": "DAT","children":[]}, 
-            ]}
-      ]
-  },
-    {"name" : "Language Tests", "children" : [
-       { "name" : "Boston Naming Test (BNT)", "children" : [] },
-       { "name" : "Token Test", "children" : [] },
-       { "name" : "Peabody Picture Vocabulary Test-Third Edition (PPVT-III)", "children" : [] }      
-    ] },
-    { "name" : "Memory Tests", "children" : [
-       { "name" : "Verbale Leer en Geheugen Taak (VLGT)", "children" : [] },
-       { "name" : "(Rey) Auditory Verbal Learning Test (AVLT)", "children" : [] },
-       { "name" : "10 Word Test (10wt)", "children" : [] },        
-       { "name" : "(Rey) Visual Design Learning Test (VDLT)", "children" : [] },
-       { "name" : "Rey Complex Figure Task (RCFT)", "children" : [] },       
-       { "name" : "Boston Naming Test (BNT)", "children" : [] },
-       { "name" : "Rivermead Behaviour Memory Test (RBMT)", "children" : [] },
-       { "name" : "Benton Visual Retention Task (BVRT)", "children" : [] },
-       { "name" : "The Enhanced Cued Recall Test (ECR)", "children" : [] },
-       { "name" : "Visual Association Test (VAT)", "children" : [] },
-       { "name" : "Corsi Block Tapping Test (Corsi)", "children" : [] }, 
-       { "name" : "Fepsy Tapping Test (Fepsy)", "children" : [] },
-       { "name" : "Self-ordered pointing task (SOP)", "children" : [] },
-       { "name" : "Location Learning Test (LLT)", "children" : [] }, 
-       { "name" : "Selective Reminding Test (SRT)", "children" : [] },
-       { "name" : "Memory Update (MU)", "children" : [] },
-       { "name" : "The Doors and People test (Doors)", "children" : [] },
-     { "name" : "Amsterdamse Korte-Termijn Geheugen Taak (AKGT)", "children" : [] },
-    ] },
-    {"name" : "Perception Tests", "children" : [
-       { "name" : "Benton’s Judgment of Line Orientation Test (JLO)", "children" : [] },
-       { "name" : "Degraded Facial Affect Recognition (DFAR)", "children" : [] },
-       { "name" : "Benton Face Recognition Test (BFRT)", "children" : [] },      
-       { "name" : "Bourdon-Wierma dot cancellation Test (BD): ", "children" : [] },
-       { "name" : "Bourdon-Vos Test (BVT)", "children" : [] }  
-    ] },
-    {"name" : "Motor/Praxis Tests", "children" : [
-       { "name" : "Grooved Pegboard (GroPeg)", "children" : [] },
-       { "name" : "Purdue Pegboard Test (PPT)", "children" : [] },
-       { "name" : "Vienna Test System (VTS)", "children" : [] },       
-       { "name" : "Beery VMI (BVMI)", "children" : [] }
-    ] },
-    {"name" : "Attention and Working Memory", "children" : [
-       { "name" : "Trail Making Task (TMT)", "children" : [] },
-       { "name" : "Stroop", "children" : [] },
-       { "name" : "TestD2", "children" : [] },       
-       { "name" : "Test battery of Attentional Performance (TAP)", "children" : [] },
-       { "name" : "Paced Auditory Serial Addition Test (PASAT)", "children" : [] },
-       { "name" : "HQ - Continuous Performance Test (HQ-CPT)", "children" : [] },  
-    ] },    
-    {"name" : "Executive Functions", "children" : [
-       { "name" : "Semantic Fluency", "children" : [] },
-       { "name" : "Letter Fluency", "children" : [] },
-       { "name" : "The Five Point Test (FPT)", "children" : [] },      
-       { "name" : "Wisconsin card sorting test (WCST)", "children" : [] },
-       { "name" : "Behavioural Assessment of the Dysexecutive Syndrome (BADS)", "children" : [] },
-       { "name" : "Ruff Figural Fluency Task (RFFT)", "children" : [] },  
-       { "name" : "Tower of London (TOL)", "children" : [] },  
-       { "name" : "Eriksen Flanker Task (FLANK)", "children" : [] },
-       { "name" : "Brixton Spatial Anticipation Test (BSAT)", "children" : [] },  
-       { "name" : "Hayling Sentence Completion Test (HSCT)", "children" : [] },  
-       { "name" : "Response shifting task (RST)", "children" : [] }
-    ] },
-    {"name" : "Cognitive screening tools / dementia screening", "children" : [
-       { "name" : "Mini-Metal state Examination (MMSE)", "children" : [] },
-       { "name" : "Benton Temporal Orientation Test (BTO)", "children" : [] },
-       { "name" : "7 minute dementia screen (SevenMin)", "children" : [] },      
-       { "name" : "Cognitieve Screening Test (CST)", "children" : [] },
-       { "name" : "Cambridge Cognitive Examination (CAMCOG)", "children" : [] },
-       { "name" : "Montreal Cognitive Assessment (MoCA)", "children" : [] },  
-       { "name" : "Mattis Dementia Scale (MDS)", "children" : [] },  
-       { "name" : "Amsterdamse Dementie Screening Test (ADS)", "children" : [] },
-       { "name" : "Cognitive Failures Questionnaire (CFQ)", "children" : [] },  
-       { "name" : "Clock drawing", "children" : [] },  
-       { "name" : "Symbol Digit Modalities Test (SDMT)", "children" : [] }
-    ] },
-    {"name" : "Social Cognition", "children" : [
-       { "name" : "Emotion Recognition Task (ERT)", "children" : [] },
-       { "name" : "Guilford’s Cartoon Predictions Test (GPT)", "children" : [] },
-       { "name" : "Happe Cartoon test (HCT)", "children" : [] },       
-       { "name" : "Reading the Mind in the Eyes test (RME)", "children" : [] },
-       { "name" : "Hinting Task (HT)", "children" : [] }
-    ] },    
-    { "name" : "Questionnaires depression/anxiety", "children" : [
-       { "name" : "Hospital Anxiety Depression Scale (HADS)", "children" : [] },
-       { "name" : "Inventory of Depressive Symptomatology (IDS)", "children" : [] },
-       { "name" : "Center for Epidemiological Studies Depression Scale (CES-D)", "children" : [] },        
-       { "name" : "Profile of Mood States (POMS)", "children" : [] },
-       { "name" : "Rand Mental Health Inventory (MHI)", "children" : [] },       
-       { "name" : "Liebowitz Social Anxiety Scale (LSAL)", "children" : [] },
-       { "name" : "The AMC Linear Disability Score (ALDS)", "children" : [] },
-       { "name" : "The Short Form (36) Health Survey (SF-36)", "children" : [] },
-       { "name" : "Symptom Checklist (SCL-90)", "children" : [] },
-       { "name" : "NEO Five Factor Inventory (NEO-FFI)", "children" : [] },
-       { "name" : "Multidimensional fatigue inventory (MFI-20)", "children" : [] }, 
-       { "name" : "Utrechtse Coping Lijst (UCL)", "children" : [] },
-       { "name" : "The World Health Organization Quality of Life (WHOQol)", "children" : [] },
-       { "name" : "Becks Anxiety Inventory (BAI)", "children" : [] }, 
-       { "name" : "Becks Depression Inventory (BDI)", "children" : [] },
-       { "name" : "Hopkins Symptoms Checklist (HSCL-25)", "children" : [] },
-       { "name" : "Cognitive Failures Questionnaire (CFQ)", "children" : [] },
-     { "name" : "Checklist Individual Strength (CIS20r)", "children" : [] },
-     { "name" : "Assesment of Depression Inventory (ADI)", "children" : [] }
-
-    ] },
-    { "name" : "ADHD / Children", "children" : [
-       { "name" : "Wender Utah ADHD Rating Scale (WURS)", "children" : [] },
-       { "name" : "Rösler ADHD questionnaire (RAQ)", "children" : [] },
-       { "name" : "Connors’ Adult ADHD Rating Scale (CAARS)", "children" : [] },       
-       { "name" : "ADHD Vragenlijst (AVL)", "children" : [] },
-       { "name" : "Vragenlijst voor Inventarisatie van Sociaal Gedrag van Kinderen (VISK)", "children" : [] },       
-       { "name" : "Strengths & Difficulties Questionnaires (SDQ)", "children" : [] },
-       { "name" : "Child Behaviour Checklist (CBCL)", "children" : [] },
-       { "name" : "Reynell Developmental Language Scales (RDLS)", "children" : [] },
-       { "name" : "Behavior Rating Inventory of Executive Function(BRIEF)", "children" : [] },
-       { "name" : "PedsQLTM 4.0", "children" : [] },
-       { "name" : "Test of Everyday Attention for Children (TEA-CH)", "children" : [] }, 
-       { "name" : "Disruptive Behavior Disorders (DBD)", "children" : [] },
-       { "name" : "Vragenlijst voor Gedragsproblemen bij Kinderen 6-16 jaar (VVGK-16)", "children" : [] },
-       { "name" : "Behavior Disorder Scale (BDS)", "children" : [] }, 
-       { "name" : "Leidse Diagnostische Test (LDT)", "children" : [] },
-       { "name" : "Children Communication Checklist (CCC)", "children" : [] },
-       { "name" : "Gedragsvragenlijst voor Kleuters (GVK)", "children" : [] }
-
-    ] },    
-];
-
-  $scope.selectOnly1Or2 = function(item, selectedItems) {
-    if (selectedItems  !== undefined && selectedItems.length >= 20) {
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  $scope.switchViewCallback = function(scopeObj) {
-      scopeObj.switchViewLabel = 'Multiple Test Selection';
-      scopeObj.inputModel = data;
-      scopeObj.selectOnlyLeafs = true;
-      print (data)
-  }
-
-  $scope.getData=function(selectedItems){
-    return(selectedItems)
-  }
-
-});
-*/
 
 app.controller('plotController', function($scope){
   /* Chart options */
