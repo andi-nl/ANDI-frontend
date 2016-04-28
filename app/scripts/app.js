e993e2d9e24e9bdf8e807a491d2e901a051ccbd3
Mateusz Kuzak <mateusz.kuzak@gmail.com>
2016-04-28 16:42:50 +0200

    Remove old deps and upgrade remaining ones

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,38 +8,35 @@
  *
  * Main module of the application.
  */
-var app =angular.module('andiApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ui.bootstrap',
-    'ivh.treeview',
-    'ui.ace',
-    'chart.js',
-    'treeGrid',
-    'ngTableToCsv',
-    'nvd3'
-  ]);
+var app = angular.module('andiApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'ui.bootstrap',
+  'ivh.treeview',
+  'ui.ace',
+  'treeGrid'
+]);
 
-  //Setting for RCtrl
+//Setting for RCtrl
 
-  app.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/diagnosis.html',
-        controller: 'TableController',
-        controllerAs: 'diagnosis'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/diagnosis.html',
+      controller: 'TableController',
+      controllerAs: 'diagnosis'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl',
+      controllerAs: 'about'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 
-  });
+});
