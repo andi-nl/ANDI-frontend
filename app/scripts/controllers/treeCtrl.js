afe5f0d923d5ba0d128feb7403f1c7f49f895018
Anand Gavai <anand.gavai@gmail.com>
2016-06-08 10:12:23 +0200

    split up controller file and removed diagnosis.js

--- app/scripts/controllers/treeCtrl.js
+++ app/scripts/controllers/treeCtrl.js
@@ -9,29 +9,7 @@
  */
 
 var defaultFolder = '2016-01-14';  //default folder name when we take test data
-/*
-  @name andiApp.controller:PanelCtrl
-  @description : change tab code made in this controller
-*/
-app.controller("PanelController",function(){
-  this.tab=1; // Default Active Tab
-  /*Tab Click Event*/
-  this.selectTab=function(setTab){
-    this.tab=setTab;
-  };
-  /*show the clicked tab panel*/
-  this.isSelected=function(checkTab){
-    return this.tab === checkTab;
-  };
-  /*previous button click event to backward one tab*/
-  this.previous =function(){
-    return this.tab= this.tab-1;
-  };
-  /*next button click event to move on tab forward*/
-  this.next=function(){
-    return this.tab = this.tab+1;
-  };
-});
+
 /*
   @name andiApp.controller:treeController
   @description : we put form submit and csv upload code here
@@ -555,8 +533,6 @@ app.controller('treeController', function($http,$scope,$timeout,$uibModal,$q,dia
       }
     });
   };
-
-
 });
 
 /*
@@ -573,40 +549,5 @@ app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance) {
 });
 
 
-/*
-  @name andiApp.controller:plotController
-  @description : put third tab chart event
-*/
-app.controller('plotController', function($scope,$http,diagnosisService){
-  //call during secont tab next button click time
-  $scope.$on('MoveToChart', function(e) {  
-    $scope.plotCtrl.FormChart();
-  });
-  /* Chart options */
-  this.FormChart=function(){
-    if($scope.patient.chart==="scatter"){ // scatter chart load
-
-    }
-    else if($scope.patient.chart==="radar"){ // radar chart load
-
-    }
-    else{ // defaut chart load
-      var patientObj = $scope.$parent.submitData;
-      var config = {
-          headers: {
-            'Content-Type': 'application/json;'
-          }
-      };
-      $http.post('http://145.100.58.103:5000/formTestScores',patientObj, config)
-      .success(function (data) {
-          console.log(data);
-          diagnosisService.lineChart(data);
-      })
-      .error(function (data) {
-        console.log(data);  
-      });
-    }
-  };
-});
 
 
