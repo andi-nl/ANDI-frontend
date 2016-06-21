eadff3b20c800384fb5341289339681885cc6056
Mateusz Kuzak <mateusz.kuzak@gmail.com>
2016-06-21 10:53:39 +0200

    Implement normcomp http request service

--- app/scripts/service/opencpu.js
+++ app/scripts/service/opencpu.js
@@ -5,8 +5,18 @@ angular
 opencpu.$inject = ['$http']
 
 function opencpu($http) {
+  var url = '192.168.99.100'
+  var port = '8004'
+  var normcompPath = '/ocpu/library/andistats/R/normcomp/json'
   var service = {
-
+    normcomp: normcomp,
   };
   return service;
+
+  function normcomp(data){
+    var normcompURL = url + ':' + port + normcompPath;
+    return $http.post(norcompURL, data).then(function(result){
+      return result.data;
+    });
+  };
 }
