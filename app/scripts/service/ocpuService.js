074e840cd70ab90b6f0ef20e4e1e5a53994a6102
Mateusz Kuzak <mateusz.kuzak@gmail.com>
2016-06-23 09:57:20 +0200

    Cleanup ocpuService
    ocpu.js uses always the same url
    that does not contain R function name.

--- app/scripts/service/ocpuService.js
+++ app/scripts/service/ocpuService.js
@@ -5,17 +5,17 @@ angular
 ocpuService.$inject = ['$http']
 
 function ocpuService($http) {
-  var url = 'http://192.168.99.100'
-  var port = '8004'
-  var normcompPath = '/ocpu/library/andistats/R'
+  var ip = 'http://192.168.99.100';
+  var port = '8004';
+  var route = '/ocpu/library/andistats/R';
+  var ocpuPath = ip + port + route;
   var service = {
     normcomp: normcomp,
   };
   return service;
 
   function normcomp(input){
-    var normcompURL = url + ':' + port + normcompPath;
-    ocpu.seturl(normcompURL);
+    ocpu.seturl(ocpuPath);
     var request = ocpu.call("normcomp", {"myJSON": JSON.stringify(input)}, function(session){
       session.getObject(function(data){
         console.log("ocpu says: " + data)
