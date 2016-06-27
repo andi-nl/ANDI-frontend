19a0b694509163272248be5869e378a553ddd32c
Mateusz Kuzak <mateusz.kuzak@gmail.com>
2016-06-27 11:46:49 +0200

    Call ocpu on plotController initiation.

--- app/scripts/controllers/plotCtrl.js
+++ app/scripts/controllers/plotCtrl.js
@@ -12,14 +12,13 @@
   @description : put third tab chart event
 */
 app.controller('plotController', function ($scope, ocpuService) {
-  //call during secont tab next button click time
-  $scope.$on('MoveToChart', function (e) {
-    $scope.plotCtrl.draw();
-  });
-  /* Chart options */
+
   this.draw = function () {
 
     var patientObj = $scope.$parent.submitData;
-    ocpuService.normcomp(patientObj)
+    ocpuService.normcomp(patientObj);
+
   };
+
+  this.draw();
 });
--- app/scripts/service/ocpuService.js
+++ app/scripts/service/ocpuService.js
@@ -8,7 +8,7 @@ function ocpuService($http) {
   var ip = 'http://192.168.99.100';
   var port = '8004';
   var route = '/ocpu/library/andistats/R';
-  var ocpuPath = ip + port + route;
+  var ocpuPath = ip + ":" + port + route;
   var service = {
     normcomp: normcomp,
   };
