91304f735e61c548abb422067423e906ab0ca612
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-07-12 21:27:56 +0200

    Add login/logout functionality to andi app

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 def index(request):
-    context = {}
-    return render(request, 'angularapp.html', context)
+    if request.user.is_authenticated():
+        return render(request, 'angularapp.html')
+    else:
+        return redirect('/accounts/signin')
