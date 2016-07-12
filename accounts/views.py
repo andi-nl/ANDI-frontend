b0bc9d06540593e5de1f84583a61904dc9721fe4
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-07-12 09:44:26 +0200

    Display angular app on index page

--- accounts/views.py
+++ accounts/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 def index(request):
     context = {}
-    return render(request, 'base.html', context)
+    return render(request, 'angularapp.html', context)
