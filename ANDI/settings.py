b12727133907463f0bcdff01c2f0e60c95a57c75
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-07-21 10:31:01 +0200

    Update settings to store migrations for sites locally
    We need to add the default domain from local settings to the
    database. This can be done using a data migration. This change
    specifies where the data migration is stored.

--- ANDI/settings.py
+++ ANDI/settings.py
@@ -86,6 +86,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ANDI.wsgi.application'
 
+MIGRATION_MODULES = {
+    'sites': 'ANDI.fixtures.sites_migrations',
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
