ed70465d343ad4e3a81221ee3e9645d0001ef6b7
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-08-03 17:35:24 +0200

    Add example of ocpu call with python
    This example uses an open ocpu server, because the andi ocpu service
    didn't work properly.

--- ocpu/views.py
+++ ocpu/views.py
@@ -1,7 +1,21 @@
+import requests
+import json
+
 from django.http import JsonResponse
 
 
 def compute(request):
-    result = {}
+    ocpuPath = 'https://public.opencpu.org/ocpu/library'
 
-    return JsonResponse(result)
+    result = requests.post(ocpuPath+'/base/R/mean/json', data={"x": '[1,2,3,4]'})
+
+    print result.status_code
+    print result.content
+    #print result.headers
+
+    if result.status_code == requests.codes.ok:
+        return JsonResponse({'result': json.loads(result.content)})
+    else:
+        return JsonResponse(result.text, safe=False)
+
+    #result.raise_for_status()
