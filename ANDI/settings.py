7986fbfd3826e2a2e88240640d7110bd86fd1e1a
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-08-04 13:12:52 +0200

    Move email settings from settings to local settings

--- ANDI/settings.py
+++ ANDI/settings.py
@@ -150,10 +150,7 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp'
 EMAIL_PORT = 25
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
+
 
 # userena
 
