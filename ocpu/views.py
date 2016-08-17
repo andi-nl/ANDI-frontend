87a4b60c48d484c378ce158647c5bf9553ca1acb
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-08-17 11:31:44 +0200

    Return error when no patient data is submited

--- ocpu/views.py
+++ ocpu/views.py
@@ -15,12 +15,19 @@ logger = logging.getLogger(__name__)
 def compute(request):
     logger.info('Called compute')
 
-    parameters = json.loads(request.body.decode('utf-8'))
+    try:
+        parameters = json.loads(request.body.decode('utf-8'))
+    except:
+        return JsonResponse({'error': 'Invalid input for ocpu.'})
 
     method_template = '/ocpu/library/andistats/R/{}/json'
 
     method = parameters.get('method')
     myJSON = json.dumps(parameters.get('input'))
+
+    if method == 'null' or myJSON == 'null':
+        return JsonResponse({'error': 'Invalid input for ocpu.'})
+
     tests = parameters.get('input').get('patientScores')[0].get('test')
     tests = [t.get('id') for t in tests]
     #myJSON = '{"settings":{"conf":"95","sig":"twoTailed","normative":"2016-01-14","chart":""},"patientScores":[{"id":"e3","age":45,"birthdate":"","testdate":"","sex":"0","education":"3","test":[{"id":"AVLT-total_1_to_5","label":"Total trial 1 to 5","Dataset":"AVLT","SPSS name":"AVLTtotal","highborder":"75.001","highweb":"75","lowborder":"15","lowweb":"0","value":45},{"id":"AVLT-recognition_1_to_5","label":"Recognition 1 to 5","Dataset":"AVLT","SPSS name":"AVLTrecognition","highborder":"30.001","highweb":"30","lowborder":"8","lowweb":"0","value":29}]},{"id":"r4","age":22,"birthdate":"","testdate":"","sex":"1","education":"5","test":[{"id":"AVLT-total_1_to_5","label":"Total trial 1 to 5","Dataset":"AVLT","SPSS name":"AVLTtotal","highborder":"75.001","highweb":"75","lowborder":"15","lowweb":"0","value":70},{"id":"AVLT-recognition_1_to_5","label":"Recognition 1 to 5","Dataset":"AVLT","SPSS name":"AVLTrecognition","highborder":"30.001","highweb":"30","lowborder":"8","lowweb":"0","value":0}]}]}'
