a4180693236a65fe330cced54ca7f13470d68a24
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-11-09 11:59:24 +0100

    Use N instead of degrees of freedom in univariate table
    Update name used to reflect the changes to the andi-ocpu service.
    
    Closes #175

--- static/app/scripts/controllers/plotCtrl.js
+++ static/app/scripts/controllers/plotCtrl.js
@@ -588,12 +588,12 @@ app.controller('plotController', function ($scope, ocpuService, svgExportService
     // columns
     var uniVarCols = [
       'id', 'plotname', 'univariatedifferences', 'univariateT',
-      'univariatedf', 'univariatep'
+      'N', 'univariatep'
     ];
 
     var uniVarColNames = [
       'patient', 'test variable', 'difference', 't-value',
-      'degrees of freedom', 'p-value'
+      'N', 'p-value'
     ];
 
     var multiVarCols = [
@@ -668,7 +668,7 @@ app.controller('plotController', function ($scope, ocpuService, svgExportService
       buttons: [
         {
           extend: 'pdf',
-          title: plotCtrl.multivariateTitle 
+          title: plotCtrl.multivariateTitle
         }
       ]
     });
