13519d973f3f5521c53f34832e8dd1e4d3bb52fd
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-11-28 15:35:38 +0100

    Update data upload service
    - show a success message if data was uploade succesfully (fixes #118)
    - show error message when data uploading fails (and the use probably
    didn't use the correct template)

--- static/app/scripts/service/dataUploadService.js
+++ static/app/scripts/service/dataUploadService.js
@@ -4,9 +4,9 @@ angular
   .module('andiApp')
   .factory('dataUploadService', dataUploadService);
 
-dataUploadService.$inject = ['$rootScope', 'toastr', 'patientDataservice', 'testTableService']
+dataUploadService.$inject = ['$rootScope', '$location', 'toastr', 'patientDataservice', 'testTableService']
 
-function dataUploadService($rootScope, toastr, patientDataservice, testTableService) {
+function dataUploadService($rootScope, $location, toastr, patientDataservice, testTableService) {
 
   var patients = [];
 
@@ -22,71 +22,81 @@ function dataUploadService($rootScope, toastr, patientDataservice, testTableServ
     var files = data;
     var r = new FileReader();
     r.onloadend = function (e) {
-      $rootScope.selectedTest = {};
-      var data = Papa.parse(e.target.result, {delimiter: '\t', dynamicTyping: true});
-      console.log(data);
+      try{
+        $rootScope.selectedTest = {};
+        var data = Papa.parse(e.target.result, {delimiter: '\t', dynamicTyping: true});
+        console.log(data);
 
-      // make array with empty patients
-      for(var i=0; i<(data.data[0].length-2); i++){
-        patients.push({});
-      }
+        // make array with empty patients
+        for(var i=0; i<(data.data[0].length-2); i++){
+          patients.push({});
+        }
 
-      // add data from csv to patients array
-      if(data.data.length > 2){
-        data.data.splice(1).forEach(function(row){
-          // TODO: check if patients with the same id are a problem (is the problem caught when the form is submitted?)
-          var fieldName = row[0];
-          var test = testTableService.findTest(fieldName, 'id');
-          if(test !== {}){
-            $rootScope.selectedTest[fieldName] = test;
-          }
-          row.splice(2).forEach(function(value, index){
-            var val;
-            var p;
-
-            if(patients[index].id === ''){
-              p = '#'+(index+1);
-            } else {
-              p = patients[index].id;
+        // add data from csv to patients array
+        if(data.data.length > 2){
+          data.data.splice(1).forEach(function(row){
+            // TODO: check if patients with the same id are a problem (is the problem caught when the form is submitted?)
+            var fieldName = row[0];
+            var test = testTableService.findTest(fieldName, 'id');
+            if(test !== {}){
+              $rootScope.selectedTest[fieldName] = test;
             }
+            row.splice(2).forEach(function(value, index){
+              var val;
+              var p;
 
-            switch(fieldName) {
-              case 'sex':
-                // sex must be 0 or 1
-                if(value !== 0 && value !== 1){
-                  toastr.warning('Patient '+p+': Invalid value for "sex". Using empty value instead.');
-                  val = '';
-                } else {
-                  // The form needs a string
-                  val = ''+value;
-                }
-                break;
-              case 'education':
-                // education must be between 1 and 7
-                if(value < 0 || value > 7){
-                  toastr.warning('Patient '+p+': Invalid value for "education". Using empty value instead.');
-                  val = '';
-                } else {
-                  // The form needs a string
-                  val = ''+value;
-                }
-                break;
-              default:
-                // remove missing values
-                if(_.includes(missingValues, value)){
-                  val = '';
-                } else {
-                  val = value;
-                }
-            }
+              if(patients[index].id === ''){
+                p = '#'+(index+1);
+              } else {
+                p = patients[index].id;
+              }
 
-            patients[index][fieldName] = val;
+              switch(fieldName) {
+                case 'sex':
+                  // sex must be 0 or 1
+                  if(value !== 0 && value !== 1){
+                    toastr.warning('Patient '+p+': Invalid value for "sex". Using empty value instead.');
+                    val = '';
+                  } else {
+                    // The form needs a string
+                    val = ''+value;
+                  }
+                  break;
+                case 'education':
+                  // education must be between 1 and 7
+                  if(value < 0 || value > 7){
+                    toastr.warning('Patient '+p+': Invalid value for "education". Using empty value instead.');
+                    val = '';
+                  } else {
+                    // The form needs a string
+                    val = ''+value;
+                  }
+                  break;
+                default:
+                  // remove missing values
+                  if(_.includes(missingValues, value)){
+                    val = '';
+                  } else {
+                    val = value;
+                  }
+              }
+
+              patients[index][fieldName] = val;
+            });
           });
-        });
+        }
+
+        toastr.success('Data uploaded successfully.');
+
+        $rootScope.$broadcast('csvUploaded', patients);
+        testTableService.setSelectedTestsWithComputedVarArguments();
+      } catch(err){
+        // invalid file
+        $rootScope.fileErr = true;
+        $location.path('/test-selection');
+
       }
 
-      $rootScope.$broadcast('csvUploaded', patients);
-      testTableService.setSelectedTestsWithComputedVarArguments();
     };
     r.readAsText(files[0]);
   }
