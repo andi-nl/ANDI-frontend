062b5c5d2773e304be1d0a712ad0614508b47114
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-11-28 14:14:49 +0100

    Move disableIntermediaryAndComputedVariables to patient data service

--- static/app/scripts/controllers/dataEntryController.js
+++ static/app/scripts/controllers/dataEntryController.js
@@ -75,72 +75,7 @@ function dataEntryController($rootScope, $scope, $location, $timeout,
    * Disable/enable input fields for computed variables/intermediary variables
    */
   dataEntry.disableIntermediaryAndComputedVariables = function(testName, patient) {
-    // testName: the name of the test for which a value was added, changed, or removed
-    // patient: patient object for which a value was added, changed, or removed
-    var computedVarArgs;
-    var useTest;
-    var value = patient[testName];
-
-    console.log(testName);
-    console.log(patient);
-    console.log(value);
-
-    if($rootScope.selectedTestsWithComputedVarArguments[testName].intermediary){
-      useTest = $rootScope.selectedTestsWithComputedVarArguments[testName].intermediaryValueFor;
-      computedVarArgs = $rootScope.selectedTestsWithComputedVarArguments[useTest].computed_variable_arguments.split(',');
-
-      // check whether either all intermediary values are empty or filled
-      var allEmpty = true;
-      var allFilled = true;
-      var args = [];
-      computedVarArgs.forEach(function(arg){
-        var v = patient[arg];
-        if(v){
-          allEmpty = false;
-          args.push(v);
-        } else {
-          allFilled = false;
-        }
-        if(!allFilled){
-          patient[useTest] = '';
-        }
-      });
-
-      if(value){
-        // an intermediary value was added (or changed); disable the input field for the computed variable
-        patient[useTest+'_disabled'] = true;
-        if(allFilled){
-          // all intermediary values required for calculating the computed value are available
-          // so, calculate the computed value
-          var input = {'compVar': useTest, 'args': args};
-          ocpuService.calccomposite(input).then(function (data) {
-            patient[useTest] = data.data.data.value;
-          });
-        }
-      } else {
-        // an intermediary value was removed
-        if(allEmpty){
-          // all intermediary values are empty, enable the input field for the computed value
-          useTest = $rootScope.selectedTestsWithComputedVarArguments[testName].intermediaryValueFor;
-          patient[useTest+'_disabled'] = false;
-        }
-      }
-    } else {
-      // we are not dealing with a value for an intermediary variable
-      // check to see whether we are dealing with a value for a computed variable
-      computedVarArgs = $rootScope.selectedTestsWithComputedVarArguments[testName].computed_variable_arguments.split(',');
-      if(computedVarArgs[0] !== ""){
-        computedVarArgs.forEach(function(arg){
-          if(value){
-            // a computed value was filled in; disable the input fields for the intermediary values
-            patient[arg+'_disabled'] = true;
-          } else {
-            // a computed value was removed; enable the input fields for the intermediary values
-            patient[arg+'_disabled'] = false;
-          }
-        });
-      }
-    }
+    patientDataservice.disableIntermediaryAndComputedVariables(testName, patient);
   };
 
   /*
