b17944e2bb371f60c25c7566818796dd95e5c169
Mateusz Kuzak <mateusz.kuzak@gmail.com>
2017-04-04 14:10:41 +0200

    Add local settings to run on localhost.
    This is the setup that should be used when not using docker
    and running the application on local host with console email backend.

---
+++ ANDI/local_settings.localhost.py
@@ -0,0 +1,33 @@
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# To generate a key in python:
+# import os
+# import binascii
+# binascii.hexlify(os.urandom(50))
+SECRET_KEY = '-(%*&koiplct1vfmiy_aovb@*ncmggq#$s%vi2b4ql8za=k2!#'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# domain the ANDI application is hosted on, e.g., www.andi.nl
+DOMAIN = ['localhost:8000']
+
+# email settings
+
+# email address account activation mails are send from and potential users can
+# use to request an account
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'user@example.com'
+
+# open cpu settings
+
+OCPU_HOST = 'localhost'
+OCPU_PORT = 8004
